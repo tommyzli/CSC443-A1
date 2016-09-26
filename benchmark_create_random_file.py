4e1bf641f8fa224924aaeee5fa35ae40bac15fee
Tommy Li <tommyli113@gmail.com>
2016-09-26 01:40:29 -0400

    Q1

---
+++ benchmark_create_random_file.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import subprocess
+
+BLOCK_SIZES = [
+    ("100", "100B"),
+    ("2048", "2KB"),
+    ("8192", "8KB"),
+    ("16384", "16KB"),
+    ("32768", "32KB"),
+    ("65536", "64KB"),
+    ("262144", "256KB"),
+    ("524288", "512KB"),
+    ("1048576", "1MB"),
+    ("3145728", "3MB"),
+]
+
+# FILE_SIZE = "10485760"  # 10 MB
+FILE_SIZE = "104857600"  # 100 MB
+
+if __name__ == "__main__":
+    for block_size, name in BLOCK_SIZES:
+        # use block size as file name
+        print("---- {} ----".format(name))
+        subprocess.call(["./create_random_file", block_size, FILE_SIZE, block_size])
+        print("------------")
